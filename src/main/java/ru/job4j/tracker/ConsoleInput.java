7faffc4392e2703a657fcbfd5872a8eec7a802de
79825 <bmchek13@gmail.com>
2022-10-28 18:40:27 +0500

    4.2. Статические методы. [#181779]

--- src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -7,12 +7,13 @@ public class ConsoleInput implements Input {
 
     @Override
     public String askStr(String question) {
-        System.out.println(question);
+        System.out.print(question);
         return scanner.nextLine();
     }
 
     @Override
     public int askInt(String question) {
+
         return Integer.parseInt(askStr(question));
     }
 }
