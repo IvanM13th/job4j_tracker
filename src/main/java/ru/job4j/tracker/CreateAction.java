d147935175befdcb27b0f324ac223ee7405b9b28
79825 <bmchek13@gmail.com>
2022-10-31 19:58:40 +0500

    5.1. Тестирование. Подготовка данных. [#182960 #356632]

--- src/main/java/ru/job4j/tracker/CreateAction.java
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -17,4 +17,3 @@ public class CreateAction implements UserAction {
         return true;
     }
 }
-
--- src/main/java/ru/job4j/tracker/DeleteAction.java
+++ src/main/java/ru/job4j/tracker/DeleteAction.java
@@ -19,4 +19,3 @@ public class DeleteAction implements UserAction {
         return true;
     }
 }
-
