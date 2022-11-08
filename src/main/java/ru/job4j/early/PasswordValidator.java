ca12849a21178d3723dafeddcbfdc2ead8b1d7a7
IvanM13th <bmchek13@gmail.com>
2022-11-08 21:32:07 +0500

    2.1. Тест на ValidateInput [#311887]

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,18 @@
+package ru.job4j.early;
+
+public class PasswordValidator {
+    public static String validate(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password can't be null");
+        }
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException("Password should be length [8, 32]");
+        }
+
+        boolean valid = false;
+        char[] array = password.toCharArray();
+
+        return null;
+    }
+
+}
