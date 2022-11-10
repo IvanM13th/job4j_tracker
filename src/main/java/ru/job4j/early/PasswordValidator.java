b37baacf1c832f005b54aa025150c226dd8037e4
IvanM13th <bmchek13@gmail.com>
2022-11-10 09:20:56 +0500

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -8,11 +8,83 @@ public class PasswordValidator {
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-
-        boolean valid = false;
         char[] array = password.toCharArray();
+        if (!HasUpperCase(array)) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (!HasLowerCase(array)) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (!HasDigit(array)) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (!HasSymbol(password)) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
+        }
+        if (!NoSubtext(password)) {
+            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+        }
+        return password;
+    }
 
-        return null;
+    public static boolean HasUpperCase(char[] array) {
+        boolean upperCase = false;
+        for (char x : array) {
+            if (Character.isUpperCase(x)) {
+                upperCase = true;
+                break;
+            }
+        }
+        return upperCase;
+    }
+
+    public static boolean HasLowerCase(char[] array) {
+        boolean lowerCase = false;
+        for (char x : array) {
+            if (Character.isLowerCase(x)) {
+                lowerCase = true;
+                break;
+            }
+        }
+        return lowerCase;
+    }
+
+    public static boolean HasDigit(char[] array) {
+        boolean hasDigit = false;
+        for (char x : array) {
+            if (Character.isDigit(x)) {
+                hasDigit = true;
+                break;
+            }
+        }
+        return hasDigit;
+    }
+
+    public static boolean HasSymbol(String password) {
+        boolean hasSymbol = false;
+        for (int i = 0; i < password.length(); i++) {
+            int code = password.codePointAt(i);
+            if (code == 36 || code == 95) {
+                hasSymbol = true;
+                break;
+            }
+        }
+        return hasSymbol;
+    }
+
+    public static boolean NoSubtext(String password) {
+        boolean noSubtext = true;
+        String toLowCasePass = password.toLowerCase();
+        int index = -1;
+        String[] subtext = {"qwerty", "12345", "password", "admin", "user"};
+        for (String s : subtext) {
+            index = toLowCasePass.indexOf(s);
+            if (index >= 0) {
+                noSubtext = false;
+                break;
+            }
+        }
+        return noSubtext;
     }
 
 }
