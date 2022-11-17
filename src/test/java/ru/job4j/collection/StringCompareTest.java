fc0b9804b11286199c5370ba535272391a5e6a4c
IvanM13th <bmchek13@gmail.com>
2022-11-17 11:19:12 +0500

    3. Компаратор для строк [#35008]

---
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -0,0 +1,95 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCompareTest {
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rst).isEqualTo(0);
+    }
+
+    @Test
+    public void whenLeftLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rst).isLessThan(0);
+    }
+
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(rst).isGreaterThan(0);
+    }
+
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst).isGreaterThan(0);
+    }
+
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Patrova",
+                "Petrov"
+        );
+        assertThat(rst).isLessThan(0);
+    }
+
+    @Test
+    public void thirdCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Pedrov"
+        );
+        assertThat(rst).isGreaterThan(0);
+    }
+
+    @Test
+    public void thirdCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Pedrova",
+                "Petrov"
+        );
+        assertThat(rst).isLessThan(0);
+    }
+    @Test
+    public void lastCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Petrof"
+        );
+        assertThat(rst).isGreaterThan(0);
+    }
+
+    @Test
+    public void lastCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrova",
+                "Petrovy"
+        );
+        assertThat(rst).isLessThan(0);
+    }
+}
