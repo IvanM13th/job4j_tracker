902a8055671e2b9b9ccfe5c749a20a6612dbee4b
IvanM13th <bmchek13@gmail.com>
2022-11-18 12:27:52 +0500

    0. Сортировка [#10096 #361596]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -35,7 +35,7 @@ public class Item {
             return false;
         }
         Item item = (Item) o;
-        return Objects.equals(name, item.name);
+        return id == item.id && Objects.equals(name, item.name);
     }
 
     @Override
