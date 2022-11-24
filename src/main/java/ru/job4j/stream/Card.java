4e1d249734dcc4e334f8d69258ab586b26f104f9
IvanM13th <bmchek13@gmail.com>
2022-11-24 09:35:20 +0500

    9. Генерирование колоды карт. FlatMap [#504873 #363419]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -1,7 +1,5 @@
 package ru.job4j.stream;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Card {
@@ -21,9 +19,19 @@ public class Card {
         this.value = value;
     }
 
+    @Override
+    public String toString() {
+        return "Card{"
+                + "suit="
+                + suit
+                + ", value="
+                + value + '}';
+    }
+
     public static void main(String[] args) {
-        List<Card> cards = Stream.of(Suit.values())
-                .flatMap(s -> Stream.of(Value.values()).map(v -> new Card(s, v)))
-                .collect(Collectors.toList());
+        Stream.of(Suit.values())
+                .flatMap(s -> Stream.of(Value.values())
+                        .map(v -> new Card(s, v)))
+                .forEach(System.out::println);
     }
 }
