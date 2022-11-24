5588a656ebc15f8973bb78de24168fbc511f7c51
IvanM13th <bmchek13@gmail.com>
2022-11-24 10:37:49 +0500

    2. Local-Variable Type Inference [#70626]

--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -6,42 +6,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PriorityQueueTest {
     @Test
     public void whenHigherPrioritySecond() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc()).isEqualTo("urgent");
     }
 
     @Test
     public void whenHigherPriorityEquals() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 5));
         queue.put(new Task("middle", 5));
-        Task result = queue.take();
+        var result = queue.take();
        assertThat(result.getDesc()).isEqualTo("low");
     }
 
     @Test
     public void whenHigherPriorityThird() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 7));
         queue.put(new Task("middle", 1));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc()).isEqualTo("middle");
     }
 
     @Test
     public void whenHigherPriorityFourth() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 7));
         queue.put(new Task("middle", 2));
         queue.put(new Task("top", 1));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc()).isEqualTo("top");
     }
 }
